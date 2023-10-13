f11d58394957214a12bc9b75d0b6584743216bc7
DaRkNiNjA0510 <113191079+DaRkNiNjA0510@users.noreply.github.com>
2023-10-13 13:45:04 +0000

    13/10/2023 Commitment

--- C0452/src/c0452/music playlist/Arraylist.java
+++ C0452/src/c0452/music playlist/Arraylist.java
@@ -1,2 +1,26 @@
 import java.util.ArrayList;
-ArrayList<String> musicList = new ArrayList<String>();
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        List<Playlist> playlist = new ArrayList<>();
+    }
+}
+//adding into the array
+public Playcount findbyplaycount(int playcount)
+  {
+  for(Playcount playcount: playcounts)
+  {
+    if(playcount.getPlaycount()==playcount)
+      return playcount;
+  }
+  return null;
+//removing from the array
+public Playcount remove (int playcount)
+  {
+  Playcount playcount=findbyplaycount(playcount);
+if(playcount!=null)
+  playcount.remove(playcount);
+else
+  System.oout.println("Playcount not found");
+  }
